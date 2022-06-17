087f482322d569d1e4d44cc86c2b1f5588b28fd2
Rajeev <scraptorajeev@gmail.com>
2022-06-17 15:04:30 +0530

    CDK Setup for Loyalty rewards

---
+++ cdk/bin/cdk.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import * as dotenv from 'dotenv';
+import { CdkStack } from '../lib/cdk-stack';
+
+dotenv.config();
+
+const appEnv = process.env.CDK_ENVIRONMENT || 'dev';
+
+const app = new cdk.App();
+new CdkStack(app, `loyalty-rewards-dapp-${appEnv}`, {
+  env: { region: 'us-east-1', account: '533793137436' },
+});
