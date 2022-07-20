4f93dcb99c8a7d810e5a2d9a5f16dcff2000a431
dasari-ananya <ananya.sgrdasari@gmail.com>
2022-07-20 16:02:03 +0530

    footer responsive

--- snet-ui/Footer/constants.tsx
+++ snet-ui/Footer/constants.tsx
@@ -48,3 +48,5 @@ const PrimaryFooterMainLinks = [
     ],
   },
 ];
+
+export default PrimaryFooterMainLinks;
--- snet-ui/Footer/index.tsx
+++ snet-ui/Footer/index.tsx
@@ -3,11 +3,9 @@ import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import Typography from '@mui/material/Typography';
-import ListItem from '@mui/material/ListItem';
-import { AIRDROP_LINKS, AIRDROP_SITE_STRING } from 'utils/airdropWindows';
 import { Container, Link, Box, Button, TextField } from '@mui/material';
 import footerStyles from './styles';
-import { LinksData } from './constants';
+import PrimaryFooterMainLinks from './constants';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -29,69 +27,82 @@ function Footer({ handleScrollToLink }: FooterProps) {
   return (
     <Container className={classes.footerMainContainer}>
       <Box className={classes.footerWrapper}>
-        <Grid container className={classes.primaryFooter}>
-          <Grid item xs={12} sm={4} className={classes.logoSection}>
-            <img alt="SingularityNET" src="SNET Logo.png" />
-            <span>Home of Benevolent AI & AGI</span>
-            <ul>
-              <li>
-                <a href="#" title="Telegram">
-                  <TelegramIcon />
-                </a>
-              </li>
-              <li>
-                <a href="#" title="Twiiter">
-                  <TwitterIcon />
-                </a>
-              </li>
-              <li>
-                <a href="#" title="LinkedIn">
-                  <LinkedInIcon />
-                </a>
-              </li>
-              <li>
-                <a href="#" title="Facebook">
-                  <FacebookIcon />
-                </a>
-              </li>
-              <li>
-                <a href="#" title="Youtube">
-                  <YouTubeIcon />
-                </a>
-              </li>
-            </ul>
-            <Box className={classes.newsLetterContainer}>
-              <span>Subscribe to Newsletter</span>
-              <Box>
-                <TextField value={email} placeholder="johndoe@gmail.com" onChange={handleEmailChange} fullWidth />
-                <Button>join</Button>
+        <Box className={classes.primaryFooter}>
+          <Box className={classes.logoSection}>
+            <Box className={classes.logo}>
+              <img alt="SingularityNET" src="SNET Logo.png" />
+              <span>Home of Benevolent AI & AGI</span>
+            </Box>
+            <Box className={classes.socialIconsNewsletterContainer}>
+              <ul>
+                <li>
+                  <a href="#" title="Telegram">
+                    <TelegramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Twiiter">
+                    <TwitterIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="LinkedIn">
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Facebook">
+                    <FacebookIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Youtube">
+                    <YouTubeIcon />
+                  </a>
+                </li>
+              </ul>
+              <Box className={classes.newsLetterContainer}>
+                <span>Subscribe to Newsletter</span>
+                <Box>
+                  <TextField value={email} placeholder="johndoe@gmail.com" onChange={handleEmailChange} fullWidth />
+                  <Button>join</Button>
+                </Box>
               </Box>
             </Box>
-          </Grid>
-          {/* {LinksData.map((linkItem) => (
-            <Grid item xs={12} sm={3} key={linkItem.header}>
-              <List
-                subheader={
-                  <Typography variant="h3">{linkItem.header}</Typography>
-                }
-              >
-                {linkItem.links.map((link) => (
-                  <ListItemButton
-                    component={link.scrollToKey ? 'b' : 'a'}
-                    href={link.url}
-                    target={link.external ? '_blank' : ''}
-                    rel={link.external ? 'noreferrer noopener' : ''}
-                    key={link.text}
-                    onClick={() => handleScrollToLink(link.scrollToKey)}
-                  >
-                    <Typography variant="body1" sx={{ m: 2, mt: 0, mb: 0 }}>
-                      {link.text}
-                    </Typography>
-                  </ListItemButton>
+          </Box>
+          <Box className={classes.footerRightSideLinks}>
+            {PrimaryFooterMainLinks.map((item) => (
+              <ul key={item.label} className={classes.footerLinksContainer}>
+                <span className={classes.footerLinksTitle}>{item.title}</span>
+                {item.children.map((child) => (
+                  <li className={classes.footerLinks}>
+                    <a
+                      href={child.link}
+                      title={child.label}
+                      target={child.internalLink ? '_self' : '_blank'}
+                      rel="noopener noreferrer"
+                    >
+                      {child.label}
+                    </a>
+                  </li>
                 ))}
-              </List>
-            </Grid>
-          ))} */}
+              </ul>
+            ))}
+          </Box>
+        </Box>
+        <Grid container className={classes.secondaryFooter}>
+          <Grid item sm={12}>
+            <span>© 2022 SingularityNET.</span>
+            <p>
+              <a href="#" title="Cookie concent policy">
+                Cookie concent policy
+              </a>{' '}
+              and{' '}
+              <a href="#" title="Privacy Policy">
+                Privacy Policy.
+              </a>
+            </p>
+          </Grid>
         </Grid>
       </Box>
     </Container>
@@ -99,11 +110,3 @@ function Footer({ handleScrollToLink }: FooterProps) {
 }
 
 export default Footer;
-
-{
-  /* <li className={classes.socialIconsLink}>
-      <a href={item.link} title={item.title} className={classes.socialIcon} target="_blank" rel="noopener noreferrer">
-        <Icon className={item.className} fontSize="large" />
-      </a>
-    </li> */
-}
