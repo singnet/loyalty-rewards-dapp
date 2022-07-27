261fb8e5f98821793867daeeb665fd61217b37cb
dasari-ananya <ananya.sgrdasari@gmail.com>
2022-07-27 09:25:07 +0530

    learn and connect with community style fixes with responsive

---
+++ snet-ui/LearnandConnect/constants.ts
@@ -0,0 +1,19 @@
+export const data = [
+  {
+    img: '/images/Whitepaper.png',
+    title: 'Read Whitepaper',
+    description:
+      'Go through the whitepaper to understand the technical information, concept and roadmap of the project.',
+  },
+  {
+    img: '/images/Telegram.png',
+    title: 'Telegram Community',
+    description:
+      'Did not find the answers you were looking for?  Reach out to us and we will response to your questions as soon as possible.',
+  },
+  {
+    img: '/images/Documentation.png',
+    title: 'View Documentation',
+    description: 'Learn more about the stages of the airdrop process and how the airdrop rewards are calculated.',
+  },
+];
